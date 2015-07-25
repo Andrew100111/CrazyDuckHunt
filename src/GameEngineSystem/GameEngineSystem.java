674a1b66d3df978b0cda0ba2e076966d045a6a96
Andrew <andyalpor@gmail.com>
2015-07-24 21:51:31 -0600

    New commit v1.1.2 Andrew - Added Packages

--- src/GameEngineSystem/GameEngineSystem.java
+++ src/GameEngineSystem/GameEngineSystem.java
@@ -3,18 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CrazyDuckHunter;
+package GameEngineSystem;
 
 /**
  *
  * @author Andrew
  */
-public class CrazyDuckHunter {
-
-    public static void main (String Args[]){
-        
-        
-        
-    }
+public class GameEngineSystem {
     
 }
