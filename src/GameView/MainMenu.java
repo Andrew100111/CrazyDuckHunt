90b8bc4df43f7a3f97dc5b93932ef2451b1134d1
Xcreed <jpm311296@gmail.com>
2015-07-25 21:10:27 -0600

    Window

--- src/GameView/MainMenu.java
+++ src/GameView/MainMenu.java
@@ -5,7 +5,11 @@
  */
 package GameView;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Label;
 import java.util.Scanner;
+import javax.swing.JFrame;
 
 /**
  *
@@ -16,15 +20,41 @@ public class MainMenu {
     
     public static void main(String[] args) {
         
+        windows();
+        
          //Gets user text
         Scanner input = new Scanner(System.in); 
         
         
         String name = input.nextLine(); 
-
+        //Keeps track of what the user entered
         System.out.println("You entered:");
         System.out.println(name);
+        
 
 }
+    
+    public static void windows() {
+        
+        Label empty = new Label();
+        Button click = new Button();
+        
+        click.setSize(20, 10);
+        
+        JFrame window = new JFrame("CDH"); //creates window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closing operation
+        window.getContentPane().add(click, BorderLayout.CENTER); 
+        
+        window.setSize(600, 400);
+        window.setLocationRelativeTo(null);     
+        window.setResizable(false);
+        
+        
+        
+        //window.pack();
+        window.setVisible(true);
+        
+        
+    }
     
 }
