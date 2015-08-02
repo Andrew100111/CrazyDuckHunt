a4f943f8c22171b1c8d619db932847968ab9d268
Xcreed <jpm311296@gmail.com>
2015-08-02 03:16:41 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/GameView/MainMenu.java

---
+++ GameEngineSystem/PatoSalvaje.java
@@ -0,0 +1,17 @@
+
+package GameEngineSystem;
+
+
+public class PatoSalvaje extends Enemy{
+    
+    
+    //int life = 1;
+    //int points = 1;
+    //int speed = 10; percentage of this duck: 90%
+    //float appearance = 100; percentage of this duck: 1% 
+    
+    public PatoSalvaje(int life, int points, int speed, float appearance) {
+        super(life, points, speed, appearance);
+    }
+    
+}
